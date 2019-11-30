64f22bc43b61fa80d83bd13771c5338b8b568c0e
Charlie Lee <charle0911@gmail.com>
2019-11-30 15:08:35 +0800

    Add first lambda example

---
+++ src/com/lambda/example/AnonymousExample.java
@@ -0,0 +1,51 @@
+package com.lambda.example;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class AnonymousExample {
+
+    private void withoutLambda() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hi! This function isn't using lambda");
+            }
+        }).start();
+    }
+
+    private void withLambda() {
+        new Thread(() ->
+            System.out.println("Hi! This function is using lambda")
+        ).start();
+    }
+
+    private void hasParameter() {
+        List<String> list = Arrays.asList("Hi", "my", "name", "is", "Charlie");
+        list.sort(new Comparator<String>(){
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.length()-s2.length();
+            }
+        });
+        System.out.println(list.toString());
+    }
+
+    private void lambdaHasParameter() {
+        List<String> list = Arrays.asList("Hi", "my", "name", "is", "Charlie");
+        list.sort((s1, s2) -> {
+                return s1.length()-s2.length();
+            });
+        System.out.println(list.toString());
+    }
+
+    public static void main(String[] args) {
+        AnonymousExample example = new AnonymousExample();
+        example.withLambda();
+        example.withoutLambda();
+        example.hasParameter();
+        example.lambdaHasParameter();
+    }
+}
